a8e9ee48fe7bf17d581a92517ebfae0f41a2c080
Spandanadornala <96777748+Spandanadornala@users.noreply.github.com>
2022-07-09 17:37:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import joblib
-model = joblib.load('MOBILE-PRICES')
+model = joblib.load('MOBILE-PRICE')
 st.title("MOBILE & IT'S PRICES")
 ip = st.text_input('Enter the mobile name')
 op = model.predict([ip])
