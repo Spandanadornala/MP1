36e57ad76c76cbeb181fb2c4b7b5c0fa09a61c16
Spandanadornala <96777748+Spandanadornala@users.noreply.github.com>
2022-07-09 17:22:54 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import joblib
+model = joblib.load('MOBILE-PRICES')
+st.title("MOBILE & IT'S PRICES")
+ip = st.text_input('Enter the mobile name')
+op = model.predict([ip])
+if st.button('Go'):
+  st.title(op[0])
